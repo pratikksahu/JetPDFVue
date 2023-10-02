cc1de5a08401f92eef8b893024d990cd4d7d36d7
pratikksahu <pratiksahu184@gmail.com>
2023-10-02 17:14:32 +0530

    Added functions descriptions

--- JetPDFVue/src/main/java/com/pratikk/jetpdfvue/state/VueResourceType.kt
+++ JetPDFVue/src/main/java/com/pratikk/jetpdfvue/state/VueResourceType.kt
@@ -5,6 +5,10 @@ import android.os.Parcelable
 import androidx.annotation.RawRes
 import kotlinx.parcelize.Parcelize
 
+/**
+ * Enum class to indicate which file is provided with VueResourceType
+ * @see VueResourceType
+ */
 @Parcelize
 enum class VueFileType:Parcelable{
     PDF,IMAGE,BASE64
@@ -12,14 +16,25 @@ enum class VueFileType:Parcelable{
 sealed class VueResourceType{
 
     /**
-     * @param uri If null then internally an empty file would be create otherwise param uri will be used
+     * @param uri If null then internally an empty file would be create otherwise param uri will be used as file
      * */
     @Parcelize
     data class BlankDocument(val uri:Uri? = null): VueResourceType(), Parcelable
 
+    /**
+     * @param uri Source file uri
+     * @param fileType Source file type
+     * @see VueFileType
+     */
     @Parcelize
     data class Local(val uri: Uri,val fileType:VueFileType = VueFileType.PDF) : VueResourceType(), Parcelable
 
+    /**
+     * @param url Source file url (Method type GET)
+     * @param fileType Source file type
+     * @see VueFileType
+     * @param headers Headers if required when fetching from url
+     */
     @Parcelize
     data class Remote(
         val url: String,
@@ -27,6 +42,11 @@ sealed class VueResourceType{
         val fileType:VueFileType
     ) : VueResourceType(), Parcelable
 
+    /**
+     * @param assetId Source asset id
+     * @param fileType Source file type
+     * @see VueFileType
+     */
     @Parcelize
     data class Asset(@RawRes val assetId: Int,val fileType:VueFileType) : VueResourceType(), Parcelable
 
